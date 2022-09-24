d81d7fe6814af8b9a4893a843cc888c01b2d93f8
Zeek <mzulkiful@yahoo.com>
2022-09-24 15:56:59 +0100

    Pages and Routes Setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+// pages and  components
+
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+
 
 function App() {
   return (
     <div className="App">
-    
+      <BrowserRouter>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path = '/signup'>
+          <Signup />
+        </Route>
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
