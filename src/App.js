5253ab9976cb91acc347bd143fb1060df0a823d7
Zeek <mzulkiful@yahoo.com>
2022-09-25 11:55:54 +0100

    Waiting for Auth to be Ready

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import Navbar from "./components/Navbar";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 
 function App() {
+
+  const {authIsReady} =useAuthContext()
+
   return (
     <div className="App">
+      {authIsReady && (
       <BrowserRouter>
       <Navbar />
       <Switch>
@@ -26,6 +31,7 @@ function App() {
         </Route>
       </Switch>
       </BrowserRouter>
+      )}
     </div>
   );
 }
