de72f46e1fd05927045127ff2a50e0f889e08764
Zeek <mzulkiful@yahoo.com>
2022-09-25 12:10:25 +0100

    Guarding each Route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 
 // pages and  components
@@ -12,7 +12,7 @@ import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
 
-  const {authIsReady} =useAuthContext()
+  const {authIsReady, user } =useAuthContext()
 
   return (
     <div className="App">
@@ -21,13 +21,16 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path='/'>
-          <Home />
+          {!user && <Redirect to='/login' />}
+          {user && <Home />}
         </Route>
         <Route path='/login'>
-          <Login />
+          {user && <Redirect to='/' />}
+          {!user &&<Login />}
         </Route>
         <Route path = '/signup'>
-          <Signup />
+        {user && <Redirect to='/' />}
+         { !user&& <Signup />}
         </Route>
       </Switch>
       </BrowserRouter>
