53f040076454b255f597026592d3398a29f8507a
Zeek <mzulkiful@yahoo.com>
2022-09-25 12:41:28 +0100

    Transaction Form Created

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,17 @@
 import styles from './Home.module.css'
 
 import React from 'react'
+import TransactionForm from './TransactionForm'
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+        <div className= {styles.content}>
+          transactionlist
+        </div>
+        <div className={styles.sidebar}>
+          <TransactionForm />
+        </div>
     </div>
   )
 }
---
+++ src/pages/home/TransactionForm.js
@@ -0,0 +1,45 @@
+
+
+import React, { useState } from 'react'
+
+export default function TransactionForm() {
+
+    const [name, setName]=useState('')
+    const [amount, setAmount] =useState('')
+
+    const handleSubmit = (e)  => {
+
+        e.preventDefault() 
+        console.log({
+            name,
+            amount
+        })
+    }
+  return (   
+   <>
+   <h3>Add a Transaction</h3>
+   <form onClick={handleSubmit}>
+    <label>
+        <span>Transaction name:</span>
+        <input 
+        type='text'
+        required
+        onChange={(e) => setName(e.target.value)}
+        value= {name}
+        />
+    </label>
+    <label>
+        <span>Amount ($):</span>
+        <input 
+        type='number'
+        required
+        onChange={(e) => setAmount(e.target.value)}
+        value= {amount}
+        />
+    </label>
+    <button className='btn'>Add Transaction</button>
+   </form>
+   </>
+  )
+}
+
