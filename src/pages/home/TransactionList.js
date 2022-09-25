69cd1b0e944bb01edbee6879ca1af2cc4a9ebb99
Zeek <mzulkiful@yahoo.com>
2022-09-25 19:53:01 +0100

    Showing listed Transactions

---
+++ src/pages/home/TransactionList.js
@@ -0,0 +1,19 @@
+// styles 
+
+import styles from './Home.module.css'
+
+import React from 'react'
+
+export default function TransactionList({transactions}) {
+  return (
+    <ul className={styles.transactions}>
+        {transactions.map((transaction) => (
+                 <li key={transaction.id}>
+                <p className={styles.name}>{transaction.name}</p>
+                <p className={styles.amount}>${transaction.amount}</p>
+                </li> 
+        ))}
+    </ul>
+  )
+}
+
